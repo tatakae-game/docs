4637b139dfa1f59fa8dc9203506d04bd6a372b25
quantumsheep <nathanael.dmc@outlook.fr>
2020-02-10 21:32:23 +0100

    feat(api): Add base

---
+++ api/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.set('trust proxy', true)
+
+const helmet = require('helmet')
+app.use(helmet())
+
+const body_parser = require('body-parser')
+app.use(body_parser.urlencoded({ extended: false }))
+app.use(body_parser.json())
+
+const routes = require('./routes')
+app.use(routes)
+
+app.use('*', (req, res) => {
+  res.status(404).send({
+    error: '404 Not Found',
+  })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Running on http://localhost:${port}`))
