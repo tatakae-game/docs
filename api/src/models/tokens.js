d834c27b7c32fb924af3e464d014765c2c07b914
quantumsheep <nathanael.dmc@outlook.fr>
2020-02-13 23:21:05 +0100

    feat(api): Add tokens check function

--- api/src/models/tokens.js
+++ api/src/models/tokens.js
@@ -9,3 +9,18 @@ exports.model = model
 
 const length = 128
 exports.length = length
+
+exports.check = async (token) => {
+  if (!token) return false
+  if ((typeof token) != 'string') return false
+  if (length != token.length) return false
+
+  try {
+    const res = await model.findOne({
+      value: token,
+      expires: { $gt: Date.now, }
+    }).exec()
+  } catch (e) {
+    return false
+  }
+}
