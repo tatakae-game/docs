a46d8c8a5e7d042a9878c5232f39157e481c2e42
quantumsheep <nathanael.dmc@outlook.fr>
2020-02-14 23:08:39 +0100

    feat(api): Add /auth/register route and its functioning

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -2,9 +2,14 @@ import { Router } from 'express'
 const router = Router()
 export default router
 
-import * as auth from '../auth'
+import argon2 from 'argon2'
 
-router.get('/auth/check', async (req, res) => {
+import * as auth from '../auth'
+import error, { ErrorsGenerator } from '../utils/error'
+
+import * as users from '../models/users'
+
+router.post('/auth/check', async (req, res) => {
   const token = req.token
 
   if (await auth.check(token)) {
@@ -14,4 +19,42 @@ router.get('/auth/check', async (req, res) => {
   } else {
     res.status(403).send(auth.invalid_token())
   }
+})
+
+router.post('/auth/register', async (req, res, next) => {
+  const connected = await auth.check(req.token)
+  if (connected) return next()
+
+  const { username, email, password } = req.body || {}
+  const errors = new ErrorsGenerator()
+
+  if (users.username_regex.test(username)) {
+    errors.assert(!users.model.exists({ username }), "This username is already used.")
+  } else {
+    errors.push("The username doesn't match the conditions.")
+  }
+
+  if (users.email_regex.test(email)) {
+    errors.assert(!users.model.exists({ email }), "This email is already used.")
+  } else {
+    errors.push("The email is not valid.")
+  }
+
+  errors.assert(users.password_regex.test(password), "The password doesn't match the conditions.")
+
+  if (errors.has_errors) {
+    return res.status(400).send({
+      errors: errors.messages,
+    })
+  }
+
+  await users.model.create({
+    username,
+    email,
+    password: await argon2.hash(password),
+  })
+
+  res.send({
+    success: true,
+  })
 })
