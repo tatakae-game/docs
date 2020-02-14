f2803ba6af07af0fea22e36d7e88607ac43d1712
quantumsheep <nathanael.dmc@outlook.fr>
2020-02-14 22:16:21 +0100

    feat(api): Add username, email and password regex

--- api/src/models/users.js
+++ api/src/models/users.js
@@ -6,3 +6,9 @@ export const model = db.model('User', {
   password: { type: String, },
   created: { type: Date, default: Date.now, },
 })
+
+export const username_regex = /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/
+
+export const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+export const password_regex = /^.{5,}$/
