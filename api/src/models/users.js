4dd1e66e3caf2e8811660ef2c0104c6022ec3eab
quantumsheep <nathanael.dmc@outlook.fr>
2020-02-14 23:07:03 +0100

    feat(api): Fix users username regex

--- api/src/models/users.js
+++ api/src/models/users.js
@@ -7,7 +7,7 @@ export const model = db.model('User', {
   created: { type: Date, default: Date.now, },
 })
 
-export const username_regex = /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/
+export const username_regex = /^[A-Za-z0-9_-]{3,20}$/
 
 export const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
