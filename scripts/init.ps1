c432298c51ec9a6dd4d2dd420951bcbfae248733
QuantumSheep <quantumsheep@outlook.fr>
2020-03-03 10:59:14 +0100

    feat(scripts): Fix github raw files URL

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -5,4 +5,4 @@ git clone "git@github.com:tatakae-game/tatakae-client.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/tatakae-admin.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/san.git" --recurse-submodules
 
-Invoke-WebRequest "https://github.com/tatakae-game/docs/blob/master/scripts/pull_all.ps1" -OutFile pull_all.ps1
+Invoke-WebRequest "https://raw.githubusercontent.com/tatakae-game/docs/master/scripts/pull_all.ps1" -OutFile pull_all.ps1
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,4 +5,4 @@ git clone "git@github.com:tatakae-game/tatakae-client.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/tatakae-admin.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/san.git" --recurse-submodules
 
-wget "https://github.com/tatakae-game/docs/blob/master/scripts/pull_all.sh" -O pull_all.sh
+wget "https://raw.githubusercontent.com/tatakae-game/docs/master/scripts/pull_all.sh" -O pull_all.sh
