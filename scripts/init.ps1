bcb93cf07a535870d58c487c010ce3ded0be73f4
QuantumSheep <quantumsheep@outlook.fr>
2020-03-03 16:03:53 +0100

    feat(docker): Add docker-compose.yml in init script

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -6,3 +6,4 @@ git clone "git@github.com:tatakae-game/tatakae-admin.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/san.git" --recurse-submodules
 
 Invoke-WebRequest "https://raw.githubusercontent.com/tatakae-game/docs/master/scripts/pull_all.ps1" -OutFile pull_all.ps1
+Invoke-WebRequest "https://raw.githubusercontent.com/tatakae-game/docs/master/configurations/docker-compose.yml" -OutFile docker-compose.yml
--- scripts/init.sh
+++ scripts/init.sh
@@ -6,3 +6,4 @@ git clone "git@github.com:tatakae-game/tatakae-admin.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/san.git" --recurse-submodules
 
 wget "https://raw.githubusercontent.com/tatakae-game/docs/master/scripts/pull_all.sh" -O pull_all.sh
+wget "https://raw.githubusercontent.com/tatakae-game/docs/master/configurations/docker-compose.yml" -O docker-compose.yml
