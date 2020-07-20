18ad6f9a330769744ca18df5ed3e4e688732f295
quantumsheep <quantumsheep@users.noreply.github.com>
2020-07-20 12:15:53 +0200

    feat: Add san-playground docker and git clone

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -5,6 +5,7 @@ git clone "git@github.com:tatakae-game/tatakae-api.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/tatakae-client.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/tatakae-admin.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/san.git" --recurse-submodules
+git clone "git@github.com:tatakae-game/san-playground.git" --recurse-submodules
 
 Invoke-WebRequest "https://raw.githubusercontent.com/tatakae-game/docs/master/scripts/pull_all.ps1" -OutFile pull_all.ps1
 Invoke-WebRequest "https://raw.githubusercontent.com/tatakae-game/docs/master/configurations/docker-compose.yml" -OutFile docker-compose.yml
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,7 @@ git clone "git@github.com:tatakae-game/tatakae-api.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/tatakae-client.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/tatakae-admin.git" --recurse-submodules
 git clone "git@github.com:tatakae-game/san.git" --recurse-submodules
+git clone "git@github.com:tatakae-game/san-playground.git" --recurse-submodules
 
 wget "https://raw.githubusercontent.com/tatakae-game/docs/master/scripts/pull_all.sh" -O pull_all.sh
 wget "https://raw.githubusercontent.com/tatakae-game/docs/master/configurations/docker-compose.yml" -O docker-compose.yml
